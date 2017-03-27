fe21dc6f9e67dfd795011df934abfe2ef00d5b32
MW <jozef.stalin@wp.pl>
2017-03-27 20:48:56 +0200

    fragment added

---
+++ app/src/main/java/com/example/rent/cardatabase/listing/ListingFragment.java
@@ -0,0 +1,72 @@
+package com.example.rent.cardatabase.listing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rent.cardatabase.MotoDatabaseOpenHelper;
+import com.example.rent.cardatabase.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by RENT on 2017-03-27.
+ */
+
+public class ListingFragment extends Fragment {
+
+    private static final String QUERY_KEY = "query_key";
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    private Unbinder unbinder;
+
+    private MotoDatabaseOpenHelper openHelper;
+
+    public static Fragment getInstance (String query) {
+        ListingFragment fragment = new ListingFragment();
+        Bundle arguments = new Bundle();
+        arguments.putString(QUERY_KEY, query);
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openHelper=new MotoDatabaseOpenHelper(getActivity());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_listing, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        unbinder = ButterKnife.bind(this, view);
+        String query = getArguments().getString(QUERY_KEY);
+        RecyclerViewCursorAdapter recyclerViewCursorAdapter = new RecyclerViewCursorAdapter();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(recyclerViewCursorAdapter);
+
+
+        recyclerViewCursorAdapter.setCursor(openHelper.searchQuerry(query));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
